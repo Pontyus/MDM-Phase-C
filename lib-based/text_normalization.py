fa4e0d8c65f61f7a349ac5fdde8d4dae7a13c962
BrendaDuan <48698833+BrendaDuan@users.noreply.github.com>
2021-05-06 23:31:55 +0800

    text normalization

---
+++ lib-based/text_normalization.py
@@ -0,0 +1,303 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 28 15:32:17 2021
+
+@author: chenchen
+"""
+#module for supressing warnings about future changes in Python:
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+#module sys helps install modules from inside Jupyter:
+import sys
+
+#html parser for digesting text taken from web-pages
+import html.parser
+from html.parser import HTMLParser
+
+#module for handling regular expressions and special characters
+import re
+
+#Natural Language ToolKit
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet as wn
+
+#modules for handling unicode characters and strings
+import unicodedata
+import string
+import pattern3
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import  CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+#pretty printing
+from pprint import pprint
+
+# Plotting tools
+import pyLDAvis
+import pyLDAvis.sklearn
+import matplotlib.pyplot as plt
+## define functions for lemmatization and HTML parsing
+## define list of lemmatization
+
+contraction_mapping = {
+"ain't": "is not",
+"aren't": "are not",
+"can't": "cannot",
+"can't've": "cannot have",
+"'cause": "because",
+"could've": "could have",
+"couldn't": "could not",
+"couldn't've": "could not have",
+"didn't": "did not",
+"doesn't": "does not",
+"don't": "do not",
+"hadn't": "had not",
+"hadn't've": "had not have",
+"hasn't": "has not",
+"haven't": "have not",
+"he'd": "he would",
+"he'd've": "he would have",
+"he'll": "he will",
+"he'll've": "he he will have",
+"he's": "he is",
+"how'd": "how did",
+"how'd'y": "how do you",
+"how'll": "how will",
+"how's": "how is",
+"I'd": "I would",
+"I'd've": "I would have",
+"I'll": "I will",
+"I'll've": "I will have",
+"I'm": "I am",
+"I've": "I have",
+"i'd": "i would",
+"i'd've": "i would have",
+"i'll": "i will",
+"i'll've": "i will have",
+"i'm": "i am",
+"i've": "i have",
+"isn't": "is not",
+"it'd": "it would",
+"it'd've": "it would have",
+"it'll": "it will",
+"it'll've": "it will have",
+"it's": "it is",
+"let's": "let us",
+"ma'am": "madam",
+"mayn't": "may not",
+"might've": "might have",
+"mightn't": "might not",
+"mightn't've": "might not have",
+"must've": "must have",
+"mustn't": "must not",
+"mustn't've": "must not have",
+"needn't": "need not",
+"needn't've": "need not have",
+"o'clock": "of the clock",
+"oughtn't": "ought not",
+"oughtn't've": "ought not have",
+"shan't": "shall not",
+"sha'n't": "shall not",
+"shan't've": "shall not have",
+"she'd": "she would",
+"she'd've": "she would have",
+"she'll": "she will",
+"she'll've": "she will have",
+"she's": "she is",
+"should've": "should have",
+"shouldn't": "should not",
+"shouldn't've": "should not have",
+"so've": "so have",
+"so's": "so as",
+"that'd": "that would",
+"that'd've": "that would have",
+"that's": "that is",
+"there'd": "there would",
+"there'd've": "there would have",
+"there's": "there is",
+"they'd": "they would",
+"they'd've": "they would have",
+"they'll": "they will",
+"they'll've": "they will have",
+"they're": "they are",
+"they've": "they have",
+"to've": "to have",
+"wasn't": "was not",
+"we'd": "we would",
+"we'd've": "we would have",
+"we'll": "we will",
+"we'll've": "we will have",
+"we're": "we are",
+"we've": "we have",
+"weren't": "were not",
+"what'll": "what will",
+"what'll've": "what will have",
+"what're": "what are",
+"what's": "what is",
+"what've": "what have",
+"when's": "when is",
+"when've": "when have",
+"where'd": "where did",
+"where's": "where is",
+"where've": "where have",
+"who'll": "who will",
+"who'll've": "who will have",
+"who's": "who is",
+"who've": "who have",
+"why's": "why is",
+"why've": "why have",
+"will've": "will have",
+"won't": "will not",
+"won't've": "will not have",
+"would've": "would have",
+"wouldn't": "would not",
+"wouldn't've": "would not have",
+"y'all": "you all",
+"y'all'd": "you all would",
+"y'all'd've": "you all would have",
+"y'all're": "you all are",
+"y'all've": "you all have",
+"you'd": "you would",
+"you'd've": "you would have",
+"you'll": "you will",
+"you'll've": "you will have",
+"you're": "you are",
+"you've": "you have"
+}
+
+## add stopwords dictionary
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+stopword_list = nltk.corpus.stopwords.words('english')
+stopword_list = stopword_list + ['mr', 'mrs', 'come', 'go', 'get',
+                                 'tell', 'listen', 'one', 'two', 'three',
+                                 'four', 'five', 'six', 'seven', 'eight',
+                                 'nine', 'zero', 'join', 'find', 'make',
+                                 'say', 'ask', 'tell', 'see', 'try', 'back',
+                                 'also','would']
+
+## split text into word tokens
+nltk.download('punkt')
+
+def tokenize_text(text):
+    tokens = nltk.word_tokenize(text) 
+    tokens = [token.strip() for token in tokens]
+    return tokens
+
+## expand the extractions, for example you've -> you have
+def expand_contractions(text, contraction_mapping):
+    
+    contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())), 
+                                      flags=re.IGNORECASE|re.DOTALL)
+    def expand_match(contraction):
+        match = contraction.group(0)
+        first_char = match[0]
+        expanded_contraction = contraction_mapping.get(match)\
+                                if contraction_mapping.get(match)\
+                                else contraction_mapping.get(match.lower())                       
+        expanded_contraction = first_char+expanded_contraction[1:]
+        return expanded_contraction
+        
+    expanded_text = contractions_pattern.sub(expand_match, text)
+    expanded_text = re.sub("'", "", expanded_text)
+    return expanded_text
+
+##Annotate text tokens with Part-Of-Speach tags:
+
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+
+def pos_tag_text(text_tokens):
+    def penn_to_wn_tags(pos_tag):
+        if pos_tag.startswith('J'):
+            return wn.ADJ
+        elif pos_tag.startswith('V'):
+            return wn.VERB
+        elif pos_tag.startswith('N'):
+            return wn.NOUN
+        elif pos_tag.startswith('R'):
+            return wn.ADV
+        else:
+            return None  
+    tagged_text = nltk.pos_tag(text_tokens)
+    tagged_lower_text = [(word.lower(), penn_to_wn_tags(pos_tag))
+                         for word, pos_tag in
+                         tagged_text]
+    return tagged_lower_text
+##lemmative words based on Part-of-speach POS tag
+wnl = WordNetLemmatizer()
+html_parser = HTMLParser()
+def lemmatize_text(text):
+    pos_tagged_text = pos_tag_text(text)
+    lemmatized_tokens = [wnl.lemmatize(word, pos_tag) if pos_tag
+                         else word                     
+                         for word, pos_tag in pos_tagged_text]
+    lemmatized_text = ' '.join(lemmatized_tokens)
+    return lemmatized_text
+ 
+##remove special characters
+def remove_special_characters(text):
+    tokens = tokenize_text(text)
+    pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))
+    filtered_tokens = filter(None, [pattern.sub(' ', token) for token in tokens])
+    filtered_text = ' '.join(filtered_tokens)
+    return filtered_text 
+##remove stopwords
+def remove_stopwords(text):
+    tokens = tokenize_text(text)
+    filtered_tokens = [token for token in tokens if token not in stopword_list]
+    filtered_text = ' '.join(filtered_tokens)    
+    return filtered_text
+
+## remove all non-text characters, like numbers :
+    
+def keep_text_characters(text):
+    filtered_tokens = []
+    tokens = tokenize_text(text)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    filtered_text = ' '.join(filtered_tokens)
+    return filtered_text
+
+##clean up url markers
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ' '.join(self.fed)
+    
+def strip_html(text):
+    html_stripper = MLStripper()
+    html_stripper.feed(text)
+    return html_stripper.get_data()
+
+## putting all functions together, define a nomalize function
+def normalize_corpus(corpus, only_text_chars=True):
+    normalized_corpus = []  
+    for index, text in enumerate(corpus):
+        text = html.unescape(text)
+        text = strip_html(text)
+        text = expand_contractions(text, contraction_mapping)
+        text = tokenize_text(text)
+        text = lemmatize_text(text)
+        text = remove_special_characters(text)
+        text = remove_stopwords(text)
+        if only_text_chars:
+            text = keep_text_characters(text)
+        #text = tokenize_text(text)
+        normalized_corpus.append(text)    
+    return normalized_corpus
+ 
+
+
